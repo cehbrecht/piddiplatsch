74eb1de786b0133882f99901cac8eb35d58ee3c2
Pingu Carsti <ehbrecht@dkrz.de>
2024-02-29 13:29:25 +0100

    removed hello conusmer

--- piddiplatsch/consumer.py
+++ piddiplatsch/consumer.py
@@ -22,18 +22,3 @@ def consume_topic():
 
     channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
     channel.start_consuming()
-
-
-def consume_hello():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
-    channel = connection.channel()
-
-    channel.queue_declare(queue="hello")
-
-    def callback(ch, method, properties, body):
-        print(f" [x] Received {body}")
-
-    channel.basic_consume(queue="hello", on_message_callback=callback, auto_ack=True)
-
-    print(" [*] Waiting for messages. To exit press CTRL+C")
-    channel.start_consuming()
--- piddiplatsch/send.py
+++ piddiplatsch/send.py
@@ -13,14 +13,3 @@ def send_topic():
     channel.basic_publish(exchange="topic_birds", routing_key=routing_key, body=message)
     print(f" [x] Sent {routing_key}:{message}")
     connection.close()
-
-
-def send_hello():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
-    channel = connection.channel()
-
-    channel.queue_declare(queue="hello")
-
-    channel.basic_publish(exchange="", routing_key="hello", body="Hello World!")
-    print(" [x] Sent 'Hello World!'")
-    connection.close()
