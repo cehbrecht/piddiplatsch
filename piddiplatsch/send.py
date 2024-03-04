b06b0ae2861960f8d725b6a395ebb6703c2e60a2
Carsten Ehbrecht <ehbrecht@dkrz.de>
2024-03-04 11:32:50 +0100

    added message option

--- piddiplatsch/send.py
+++ piddiplatsch/send.py
@@ -2,14 +2,14 @@
 import pika
 
 
-def send_topic():
+def send_topic(message=None):
     connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
     channel = connection.channel()
 
     channel.exchange_declare(exchange="topic_birds", exchange_type="topic")
 
     routing_key = "bird.penguin"
-    message = "Hello World!"
+    message = message or "Hello World!"
     channel.basic_publish(exchange="topic_birds", routing_key=routing_key, body=message)
     print(f" [x] Sent {routing_key}:{message}")
     connection.close()
