4779ef64c8424456ae26f93c4c1e18cc56ae2c36
Pingu Carsti <ehbrecht@dkrz.de>
2024-03-04 16:28:32 +0100

    added start_consuming

--- piddiplatsch/consumer.py
+++ piddiplatsch/consumer.py
@@ -6,34 +6,37 @@ from piddiplatsch.processor import get_message_processor
 def do_consume(queue, type=None):
     c = PIDConsumer(queue, type)
     c.open_connection()
+    c.start_consuming()
 
 
 class PIDConsumer:
     def __init__(self, queue, type=None):
         self.queue = queue
         self.type = type
+        self.channel = None
 
     def open_connection(self):
         connection = pika.BlockingConnection(
             pika.ConnectionParameters(host="localhost")
         )
-        channel = connection.channel()
+        self.channel = connection.channel()
 
-        channel.exchange_declare(exchange="topic_birds", exchange_type="topic")
+        self.channel.exchange_declare(exchange="topic_birds", exchange_type="topic")
 
-        channel.queue_declare(self.queue, exclusive=True)
+        self.channel.queue_declare(self.queue, exclusive=True)
 
         binding_key = "bird.*"
-        channel.queue_bind(
+        self.channel.queue_bind(
             exchange="topic_birds", queue=self.queue, routing_key=binding_key
         )
 
+    def start_consuming(self):
         print(" [*] Waiting for birds. To exit press CTRL+C")
 
-        channel.basic_consume(
+        self.channel.basic_consume(
             queue=self.queue, on_message_callback=self.on_message, auto_ack=True
         )
-        channel.start_consuming()
+        self.channel.start_consuming()
 
     def on_message(self, ch, method, properties, body):
         # print(f" [x] {method.routing_key}:{body}")
