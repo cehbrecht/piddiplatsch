450f309d3ec091984d8e7ffc6bb33a93f2f412aa
Pingu Carsti <ehbrecht@dkrz.de>
2024-03-19 17:37:01 +0100

    added missing drs_id test

--- tests/test_cmip6.py
+++ tests/test_cmip6.py
@@ -1,5 +1,8 @@
 import json
 from piddiplatsch.handler import filter_handlers
+from jsonschema.exceptions import ValidationError
+
+import pytest
 
 
 def test_cmip6_map_file():
@@ -36,3 +39,22 @@ def test_cmip6_map_dataset():
     }
     data = json.dumps(msg)
     handler.process_message(data)
+
+
+def test_cmip6_map_dataset_missing_drs_id():
+    handler = filter_handlers(["cmip6"])[0]
+    msg = {
+        "handle": "21.t14996/testcase200",
+        "aggregation_level": "DATASET",
+        "operation": "publish",
+        "is_replica": False,
+        "version_number": 2016,
+        # "drs_id": "foo/bar/baz/drs/tc200",
+        "files": ["hdl:21.t14996/snake", "hdl:21.t14996/monkey"],
+        "message_timestamp": "2000-10-10T10:00:00.000000+00:00",
+    }
+    data = json.dumps(msg)
+
+    with pytest.raises(Exception) as excinfo:
+        handler.process_message(data)
+    assert "'DRS_ID' is a required property" in str(excinfo.value)
