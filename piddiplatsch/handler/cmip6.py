f05c1dc70dc1f51e6d5bbfdc4dd513873dafc0a8
Pingu Carsti <ehbrecht@dkrz.de>
2024-03-19 17:19:02 +0100

    updated cmip6 schema with dataset

--- piddiplatsch/handler/cmip6.py
+++ piddiplatsch/handler/cmip6.py
@@ -6,71 +6,84 @@ CHECKSUM_METHODS = ["MD5", "SHA256"]
 
 SCHEMA = {
     "$schema": "https://json-schema.org/draft/2020-12/schema",
-    "title": "WDCC",
-    "description": "A handle record schema for WDCC.",
+    "title": "CMIP6 Schema",
+    "description": "A handle record schema for CMIP6.",
     "type": "object",
     "properties": {
         "HANDLE": {
             "description": "Handle identifier. Example: 21.T14996/TESTCASE100",
             "type": "string",
         },
-        "AGGREGATION_LEVEL": {"enum": AGGREGATION_LEVELS},
-        "FILE_NAME": {
-            "type": "string",
-            "minLength": 1,
-            "maxLength": 200,
-        },
-        "FILE_SIZE": {
-            "type": "integer",
-            "minimum": 0,
-        },
-        "FILE_VERSION": {
-            "type": "integer",
-            "minimum": 0,
-        },
-        "CHECKSUM": {"type": "string"},
-        "CHECKSUM_METHOD": {"enum": CHECKSUM_METHODS},
         "FIXED_CONTENT": {"type": "boolean"},
         "URL": {
             "description": "URL of the landing page",
             "type": "string",
             "format": "uri",
         },
-        "URL_ORIGINAL_DATA": {
-            "type": "string",
-            "format": "uri",
-        },
-        "URL_REPLICA": {
-            "type": "string",
-            "format": "uri",
-        },
-        "IS_PART_OF": {
-            "type": "string",
-            "format": "uri",
-        },
-        "DRS_ID": {
-            "type": "string",
-        },
-        "VERSION_NUMBER": {
-            "type": "string",
-        },
-        "HOSTING_NODE": {
-            "type": "string",
-        },
-        "REPLICA_NODE": {
-            "type": "string",
-        },
-        "HAS_PARTS": {
-            "type": "string",
-        },
-        "REPLACES": {
-            "type": "string",
-        },
-        "REPLACED_BY": {
-            "type": "string",
-        },
+        "AGGREGATION_LEVEL": {"enum": AGGREGATION_LEVELS},
     },
-    "required": ["URL", "AGGREGATION_LEVEL", "FILE_NAME"],
+    "required": ["URL", "AGGREGATION_LEVEL"],
+    "if": {
+        "properties": {"AGGREGATION_LEVEL": {"const": "FILE"}},
+    },
+    "then": {
+        "properties": {
+            "FILE_NAME": {
+                "type": "string",
+                "minLength": 1,
+                "maxLength": 200,
+            },
+            "FILE_SIZE": {
+                "type": "integer",
+                "minimum": 0,
+            },
+            "FILE_VERSION": {
+                "type": "integer",
+                "minimum": 0,
+            },
+            "CHECKSUM": {"type": "string"},
+            "CHECKSUM_METHOD": {"enum": CHECKSUM_METHODS},
+            "URL_ORIGINAL_DATA": {
+                "type": "string",
+                "format": "uri",
+            },
+            "URL_REPLICA": {
+                "type": "string",
+                "format": "uri",
+            },
+            "IS_PART_OF": {
+                "type": "string",
+            },
+        },
+        "required": ["FILE_NAME"],
+    },
+    "else": {
+        "properties": {
+            "DRS_ID": {
+                "type": "string",
+            },
+            "VERSION_NUMBER": {
+                "type": "integer",
+                "minimum": 0,
+            },
+            "HOSTING_NODE": {
+                "type": "string",
+            },
+            "REPLICA_NODE": {
+                "type": "string",
+            },
+            "HAS_PARTS": {
+                "type": "string",
+            },
+            "REPLACES": {
+                "type": "string",
+            },
+            "REPLACED_BY": {
+                "type": "string",
+            },
+        },
+        "required": ["DRS_ID"],
+    },
 }
 
 
@@ -85,6 +98,7 @@ class CMIP6Handler(MessageHandler):
     def map(self, handle, data):
         record = {
             "URL": f"https://handle-esgf.dkrz.de/lp/{handle}",
+            "FIXED_CONTENT": data.get("fixed_content"),
             "AGGREGATION_LEVEL": data.get("aggregation_level"),
             "FILE_NAME": data.get("file_name"),
             "FILE_SIZE": data.get("file_size"),
@@ -94,6 +108,9 @@ class CMIP6Handler(MessageHandler):
             "CHECKSUM_METHOD": data.get("checksum_method"),
             "URL_ORIGINAL_DATA": data.get("url_original_data"),
             "URL_REPLICA": data.get("url_replica"),
+            "DRS_ID": data.get("drs_id"),
+            "VERSION_NUMBER": data.get("version_number"),
+            "HAS_PARTS": data.get("has_parts"),
         }
         # remove empty values
         for key in list(record.keys()):
--- tests/test_cmip6.py
+++ tests/test_cmip6.py
@@ -20,3 +20,19 @@ def test_cmip6_map_file():
     }
     data = json.dumps(msg)
     handler.process_message(data)
+
+
+def test_cmip6_map_dataset():
+    handler = filter_handlers(["cmip6"])[0]
+    msg = {
+        "handle": "21.t14996/testcase200",
+        "aggregation_level": "DATASET",
+        "operation": "publish",
+        "is_replica": False,
+        "version_number": 2016,
+        "drs_id": "foo/bar/baz/drs/tc200",
+        "files": ["hdl:21.t14996/snake", "hdl:21.t14996/monkey"],
+        "message_timestamp": "2000-10-10T10:00:00.000000+00:00",
+    }
+    data = json.dumps(msg)
+    handler.process_message(data)
