9f5506f39872609e0178e6162164cbb2bbc49480
Pingu Carsti <ehbrecht@dkrz.de>
2024-03-19 17:39:02 +0100

    added test for missing file_name

--- tests/test_cmip6.py
+++ tests/test_cmip6.py
@@ -25,6 +25,29 @@ def test_cmip6_map_file():
     handler.process_message(data)
 
 
+def test_cmip6_map_file_missing_file_name():
+    handler = filter_handlers(["cmip6"])[0]
+    msg = {
+        "handle": "21.t14996/testcase100",
+        "aggregation_level": "FILE",
+        "operation": "publish",
+        "is_replica": False,
+        # "file_name": "temperature.nc",
+        "file_size": 1000,
+        "checksum": "ch123abc",
+        "checksum_method": "MD5",
+        "url_original_data": "https://dkrz.de/baz/temperature.nc",
+        "file_version": 1,
+        "message_timestamp": "2000-10-10T10:00:00.000000+00:00",
+        "parent_dataset": "hdl:21.t14996/cucumber",
+    }
+    data = json.dumps(msg)
+
+    with pytest.raises(Exception) as excinfo:
+        handler.process_message(data)
+    assert "'FILE_NAME' is a required property" in str(excinfo.value)
+
+
 def test_cmip6_map_dataset():
     handler = filter_handlers(["cmip6"])[0]
     msg = {
