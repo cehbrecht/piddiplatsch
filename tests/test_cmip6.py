c121d46894fb0a94fef4280ebf2223f365456839
Pingu Carsti <ehbrecht@dkrz.de>
2024-03-20 17:17:31 +0100

    update cmip6 test

--- tests/test_cmip6.py
+++ tests/test_cmip6.py
@@ -1,80 +1,70 @@
+import copy
+import json
 from piddiplatsch.handler import filter_handlers
 from jsonschema.exceptions import ValidationError
 
 import pytest
 
+TEST_FILE_1 = {
+    "handle": "21.t14996/testcase100",
+    "aggregation_level": "FILE",
+    "operation": "publish",
+    "is_replica": False,
+    "file_name": "temperature.nc",
+    "file_size": 1000,
+    "checksum": "ch123abc",
+    "checksum_method": "MD5",
+    "url_original_data": "https://dkrz.de/baz/temperature.nc",
+    "file_version": 1,
+    "message_timestamp": "2000-10-10T10:00:00.000000+00:00",
+    "parent_dataset": "hdl:21.t14996/cucumber",
+}
 
-def test_cmip6_map_file():
+TEST_DATASET_1 = {
+    "handle": "21.t14996/testcase200",
+    "aggregation_level": "DATASET",
+    "operation": "publish",
+    "is_replica": False,
+    "version_number": 2016,
+    "drs_id": "foo/bar/baz/drs/tc200",
+    "files": ["hdl:21.t14996/snake", "hdl:21.t14996/monkey"],
+    "message_timestamp": "2000-10-10T10:00:00.000000+00:00",
+}
+
+
+def test_map_file():
     handler = filter_handlers(["cmip6"])[0]
-    data = {
-        "handle": "21.t14996/testcase100",
-        "aggregation_level": "FILE",
-        "operation": "publish",
-        "is_replica": False,
-        "file_name": "temperature.nc",
-        "file_size": 1000,
-        "checksum": "ch123abc",
-        "checksum_method": "MD5",
-        "url_original_data": "https://dkrz.de/baz/temperature.nc",
-        "file_version": 1,
-        "message_timestamp": "2000-10-10T10:00:00.000000+00:00",
-        "parent_dataset": "hdl:21.t14996/cucumber",
-    }
-    record = handler.map(data)
+    record = handler.map(TEST_FILE_1)
     assert record["AGGREGATION_LEVEL"] == "FILE"
 
 
-def test_cmip6_map_file_missing_file_name():
+def test_map_file_missing_file_name():
     handler = filter_handlers(["cmip6"])[0]
-    data = {
-        "handle": "21.t14996/testcase100",
-        "aggregation_level": "FILE",
-        "operation": "publish",
-        "is_replica": False,
-        # "file_name": "temperature.nc",
-        "file_size": 1000,
-        "checksum": "ch123abc",
-        "checksum_method": "MD5",
-        "url_original_data": "https://dkrz.de/baz/temperature.nc",
-        "file_version": 1,
-        "message_timestamp": "2000-10-10T10:00:00.000000+00:00",
-        "parent_dataset": "hdl:21.t14996/cucumber",
-    }
+    data = copy.deepcopy(TEST_FILE_1)
+    del data["file_name"]
 
     with pytest.raises(ValidationError) as excinfo:
         handler.map(data)
     assert "'FILE_NAME' is a required property" in str(excinfo.value)
 
 
-def test_cmip6_map_dataset():
+def test_map_dataset():
     handler = filter_handlers(["cmip6"])[0]
-    data = {
-        "handle": "21.t14996/testcase200",
-        "aggregation_level": "DATASET",
-        "operation": "publish",
-        "is_replica": False,
-        "version_number": 2016,
-        "drs_id": "foo/bar/baz/drs/tc200",
-        "files": ["hdl:21.t14996/snake", "hdl:21.t14996/monkey"],
-        "message_timestamp": "2000-10-10T10:00:00.000000+00:00",
-    }
-    record = handler.map(data)
+    record = handler.map(TEST_DATASET_1)
     assert record["AGGREGATION_LEVEL"] == "DATASET"
 
 
-def test_cmip6_map_dataset_missing_drs_id():
+def test_map_dataset_missing_drs_id():
     handler = filter_handlers(["cmip6"])[0]
-    data = {
-        "handle": "21.t14996/testcase200",
-        "aggregation_level": "DATASET",
-        "operation": "publish",
-        "is_replica": False,
-        "version_number": 2016,
-        # "drs_id": "foo/bar/baz/drs/tc200",
-        "files": ["hdl:21.t14996/snake", "hdl:21.t14996/monkey"],
-        "message_timestamp": "2000-10-10T10:00:00.000000+00:00",
-    }
+    data = copy.deepcopy(TEST_DATASET_1)
+    del data["drs_id"]
 
     with pytest.raises(ValidationError) as excinfo:
         handler.map(data)
     assert "'DRS_ID' is a required property" in str(excinfo.value)
+
+
+def test_process_message_dataset():
+    handler = filter_handlers(["cmip6"])[0]
+    msg = json.dumps(TEST_FILE_1)
+    handler.process_message(msg, dry_run=True)
